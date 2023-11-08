1a73bd5b05abd776097d241ff7412a60fab68f84
rejaul921 <rejaulkarim921@gmail.com>
2023-11-09 03:46:25 +0400

    after returning a book deleting the book from server datacollection borrowed book

--- index.js
+++ index.js
@@ -122,6 +122,13 @@ async function run() {
       const books = await cursor.toArray();
       res.send(books);
     })
+    // deleting book from borrowedBooks
+    app.delete('/myBorrowed/:id', async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)}
+      const result= await borrowedBooks.deleteOne(query);
+      res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
