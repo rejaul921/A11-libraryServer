e4507b31c5a0cf6b07afbf0b32e3597705f91595
rejaul921 <rejaulkarim921@gmail.com>
2023-11-09 17:28:44 +0400

    deployed to vercel

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // creating Database
     const database = client.db("booksDB");
     const allBooks = database.collection("allBooks");
